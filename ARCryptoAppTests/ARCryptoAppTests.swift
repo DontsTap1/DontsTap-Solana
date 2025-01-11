2391be79bc888b930b021a147cdfb3726b3f8fac
Ivan <iv.tkachenko17@gmail.com>
2025-01-11 13:07:02 +0300

    Initial Commit

---
+++ ARCryptoAppTests/ARCryptoAppTests.swift
@@ -0,0 +1,17 @@
+//
+//  ARCryptoAppTests.swift
+//  ARCryptoAppTests
+//
+//  Created by Ivan Tkachenko on 11.01.2025.
+//
+
+import Testing
+@testable import ARCryptoApp
+
+struct ARCryptoAppTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
