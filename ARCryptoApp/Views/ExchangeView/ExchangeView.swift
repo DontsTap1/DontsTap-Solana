712a6bdd8e35f27aa5030a492592a540138a8a5d
SteadyCoder <iv.tkachenko17@gmail.com>
2025-04-08 14:08:19 +0300

    Simplified implementation of web view based exchange/currency calculator.

--- ARCryptoApp/Views/ExchangeView/ExchangeView.swift
+++ ARCryptoApp/Views/ExchangeView/ExchangeView.swift
@@ -9,50 +9,16 @@ import SwiftUI
 @preconcurrency import WebKit
 
 struct ExchangeView: View {
-    let jsWidgetHTML = """
-        <html>
-        <head>
-            <meta name="viewport" content="width=device-width, initial-scale=1.0">
-            <style>
-                html, body {
-                    margin: 0;
-                    padding: 0;
-                    height: 100%;
-                    width: 100%;
-                    display: flex;
-                    justify-content: center;
-                    align-items: center;
-                    background-color: transparent;
-                }
-                iframe {
-                    width: 100%;
-                    height: 100%;
-                    border-radius: 32px;
-                    border: none;
-                }
-            </style>
-        </head>
-        <body>
-            <iframe src="https://guardarian.com/calculator/v1?partner_api_token=ff91b7de-a2d8-4a58-8384-338151c53cb8&type=narrow&swap_enabled=true&default_crypto_currency=SOL&body_background=rgb(72,28,83)&calc_background=rgb(72,28,83)&button_background=rgb(75,138,197)&button_background_disabled=rgb(75,138,187)" 
-             allowfullscreen style="background: transparent;">
-            </iframe>
-        </body>
-        </html>
-        """
-
     @State private var isLoading: Bool = true
 
     var body: some View {
-        WebView(htmlString: jsWidgetHTML, isLoading: $isLoading)
-            .frame(maxWidth: .infinity)
-            .frame(maxHeight: .infinity) // Adjust to fit your widget size
+        WebView(isLoading: $isLoading)
             .ignoresSafeArea()
             .loadingView(isPresented: $isLoading)
     }
 }
 
 private struct WebView: UIViewRepresentable {
-    let htmlString: String
     @Binding var isLoading: Bool  // Track loading state
 
     func makeCoordinator() -> Coordinator {
@@ -67,7 +33,10 @@ private struct WebView: UIViewRepresentable {
         webView.scrollView.backgroundColor = .black
         webView.allowsBackForwardNavigationGestures = true
 
-        webView.loadHTMLString(htmlString, baseURL: nil)
+        if let url = URL(string: "https://guardarian.com/calculator/v1?partner_api_token=ff91b7de-a2d8-4a58-8384-338151c53cb8&type=narrow&swap_enabled=true&default_crypto_currency=SOL&body_background=rgb(72,28,83)&calc_background=rgb(72,28,83)&button_background=rgb(75,138,197)&button_background_disabled=rgb(75,138,187)") {
+            let urlRequest = URLRequest(url: url)
+            webView.load(urlRequest)
+        }
         return webView
     }
 
